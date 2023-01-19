4dec0a747845f9bbb1375d61cdf546328336ee03
pavelmezin <paulmezoni@gmail.com>
2023-01-19 13:13:56 +0300

    branch-8

--- src/marcus.java
+++ src/marcus.java
@@ -1,2 +1,9 @@
 public class marcus {
+    public static void main(Integer[] args) {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
 }
